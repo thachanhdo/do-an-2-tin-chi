fafadc605659fd09b7824bf2bf33314dd6608ef2
mons <dangminh200320@hotmail.com>
2026-01-04 14:05:52 +0700

    fix: change to CompressedStaticFilesStorage to avoid source map error

--- ShopDongHo/ShopDongHo/settings.py
+++ ShopDongHo/ShopDongHo/settings.py
@@ -140,7 +140,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
